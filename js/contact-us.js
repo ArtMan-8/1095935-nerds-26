0c3762d5526d33ea7312cbc7f3b6d632c35e3eda
Art <ya.artriches@ya.ru>
2019-10-19 22:31:41 +0300

    contact-us добавил js для открытия и закрытия формы обратной связи

---
+++ js/contact-us.js
@@ -0,0 +1,52 @@
+var contactShow = document.querySelector('.contacts__link');
+var formContacts = document.querySelector('.form-contacts');
+var btnSubmit = formContacts.querySelector('.form-contacts__submit');
+var btnClose = formContacts.querySelector('.form-contacts__close')
+
+var contactName = formContacts.querySelector('#form-name');
+var contactEmail = formContacts.querySelector('#form-email');
+var contactMessage = formContacts.querySelector('#form-message');
+
+var isStorageSupport = true;
+var storage = '';
+
+/* Доступен ли localStorage */
+try {
+  storage = localStorage.getItem('contactName');
+} catch (err) {
+  isStorageSupport = false;
+};
+
+/* Открытие и закрытие формы,
+сохранение данных в localStorage */
+contactShow.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  formContacts.classList.add('form-contacts--show');
+  contactName.focus();
+
+  contactName.value = localStorage.getItem('contactName');
+  contactEmail.value = localStorage.getItem('contactEmail');
+});
+
+/* Загрузках данных из localStorage, если доступно */
+formContacts.addEventListener('submit', function(evt) {
+  if (isStorageSupport) {
+    localStorage.setItem('contactName', contactName.value);
+    localStorage.setItem('contactEmail', contactEmail.value);
+  }
+});
+
+/* Закрытие формы кнопкой и клавишой ESC */
+btnClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  formContacts.classList.remove('form-contacts--show');
+});
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    if (formContacts.classList.contains('form-contacts--show')) {
+      evt.preventDefault();
+      formContacts.classList.remove('form-contacts--show');
+    }
+  }
+});
